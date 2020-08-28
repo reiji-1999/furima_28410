2b9c653c73e7f8557611dc9accd994547a29b25e
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-08-28 20:46:56 +0900

    ユーザー情報の保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :name_reading_family_name, :name_reading_first_name, :birthday])
+  end
 end
