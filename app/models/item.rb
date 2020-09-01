e435e78f54a147e54214805db03377546edfd431
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-09-01 19:35:42 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+
+  validates :name, :description, :category_id, :status_id, :burden_id, :area_id, :days_id, :price, :image, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :burden_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 1 } 
+  validates :days_id, numericality: { other_than: 1 } 
+
   belongs_to :user
   has_one :management
+  has_one_attached :image
 end
