45fc006e6824bc782d70134614049842f476a136
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-09-04 12:59:22 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,39 @@
 class ItemsController < ApplicationController
-   before_action :authenticate_user!, except: :index
-def index
-  @items = Item.all.order("created_at DESC")
-end
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy]
 
-def new
-  @item = Item.new
-end
-
-def create 
-  @item = Item.new(item_params)
-  if @item.save
-    redirect_to root_path
-  else
-    render :new
+  def index
+    @items = Item.all.order("created_at DESC")
   end
-end
 
-private
-def item_params
-  params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
-end
+  def new
+    @item = Item.new
+  end
+
+  def create 
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+
+  end
+
+  def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
