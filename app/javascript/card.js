663158528529660ee137e14e7d85889879e138cc
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-09-09 20:11:38 +0900

    デバック用の記述削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,7 +15,6 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
         const token = response.id;
@@ -30,7 +29,6 @@ const pay = () => {
 
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
-        console.log("OK")
       } else {
       }
       form.commit.disabled = false;
