ba28753457c0aa8993c398359f89add1df2e1a06
reiji-1999 <68744130+reiji-1999@users.noreply.github.com>
2020-09-11 22:01:24 +0900

    購入者の情報送信の修正

--- app/forms/buyer_management.rb
+++ app/forms/buyer_management.rb
@@ -3,7 +3,7 @@ class BuyerManagement
   attr_accessor :postalcode, :area_id, :city, :housenumber, :buildingname, :phonenumber, :token, :user_id, :item_id
 
   POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
-  
+
   with_options presence: true do
     validates :postalcode, format: { with: POSTAL_CODE_REGEX }
     validates :area_id, numericality: { other_than: 1 } 
@@ -16,7 +16,7 @@ class BuyerManagement
   end
 
   def save
-    buyer = Buyer.create(postalcode: postalcode, area_id: area_id, city: city, housenumber: housenumber, buildingname: buildingname, phonenumber: phonenumber)
+    buyer = Buyer.create(postalcode: postalcode, area_id: area_id, city: city, housenumber: housenumber, buildingname: buildingname, phonenumber: phonenumber, management_id: management_id)
     Management.create(user_id: user_id, item_id: item_id)
   end
 end
